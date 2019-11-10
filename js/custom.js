7c58b512ae694069bbdca0f96cb7c0fe790dbd1b
nicholaschun <nicholaschunrayne@gmail.com>
2019-11-10 23:39:26 +0000

    added responsive

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(".phone-menu").click(function() {
+        if($(".navigation-container-phone").css('display') == 'none'){
+            $(".navigation-container-phone").css({'display':'block'})
+        } else {
+            $(".navigation-container-phone").css({'display':'none'})
+        }
+    });
+
+    $("#menu-icon").click(function () {
+        $("#nav-bar").fadeToggle('fast');
+    });
+});
+
+

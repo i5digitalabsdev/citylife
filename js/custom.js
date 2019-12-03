e9021b3ee67bdf1714a961eef2c0e7aed5490927
nicholaschun <nicholaschunrayne@gmail.com>
2019-12-03 12:48:23 +0000

    major changes

--- js/custom.js
+++ js/custom.js
@@ -25,7 +25,114 @@ $('.owl-one').owlCarousel({
     items:1,
     nav:false,
     dots:true,
-    center:true
+    center:true,
 })
+
+$('.owl-three').owlCarousel({
+    loop:true,
+    items:1,
+    nav:false,
+    dots:true,
+    center:true,
+})
+
+$('.owl-four').owlCarousel({
+    loop:true,
+    items:1,
+    nav:false,
+    dots:true,
+    center:true,
+})
+
+$('.owl-five').owlCarousel({
+    loop:true,
+    items:1,
+    nav:false,
+    dots:true,
+    center:true,
+})
+
+$('.owl-six').owlCarousel({
+    loop:true,
+    items:1,
+    nav:false,
+    dots:true,
+    center:true,
+})
+
+$('.owl-seven').owlCarousel({
+    loop:true,
+    items:1,
+    nav:false,
+    dots:true,
+    center:true,
+})
+
+
+$('.owl-eight').owlCarousel({
+    loop:true,
+    items:1,
+    nav:false,
+    dots:true,
+    center:true,
+})
+
+
+
+var owl = $('.owl-one');
+owl.owlCarousel();
+$('.cust-next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+$('.cust-prev').click(function() {
+    owl.trigger('prev.owl.carousel', [300]);
+})
+
+var owl1 = $('.owl-three');
+owl1.owlCarousel();
+$('.cust-next1').click(function() {
+    owl1.trigger('next.owl.carousel');
+})
+$('.cust-prev1').click(function() {
+    owl1.trigger('prev.owl.carousel', [300]);
+})
+
+
+var owl2 = $('.owl-four');
+owl2.owlCarousel();
+$('.cust-next2').click(function() {
+    owl2.trigger('next.owl.carousel');
+})
+$('.cust-prev2').click(function() {
+    owl2.trigger('prev.owl.carousel', [300]);
+})
+
+var owl3 = $('.owl-five');
+owl3.owlCarousel();
+$('.cust-next3').click(function() {
+    owl3.trigger('next.owl.carousel');
+})
+$('.cust-prev3').click(function() {
+    owl3.trigger('prev.owl.carousel', [300]);
+})
+
+var owl4 = $('.owl-six');
+owl4.owlCarousel();
+$('.cust-next4').click(function() {
+    owl4.trigger('next.owl.carousel');
+})
+$('.cust-prev4').click(function() {
+    owl4.trigger('prev.owl.carousel', [300]);
+})
+
+var owl5 = $('.owl-seven');
+owl5.owlCarousel();
+$('.cust-next5').click(function() {
+    owl5.trigger('next.owl.carousel');
+})
+$('.cust-prev5').click(function() {
+    owl5.trigger('prev.owl.carousel', [300]);
+})
+
 
 
